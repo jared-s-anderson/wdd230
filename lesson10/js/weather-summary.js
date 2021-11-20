fd1365380f9e9db2aaeafc02e3c59495897df3c0
jaredsanderson <jaredsa@byui.edu>
2021-11-20 15:33:05 -0700

    Updated

---
+++ lesson10/js/weather-summary.js
@@ -0,0 +1,32 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=ab78af5b8d06381c9785194b99f21976";
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        
+        let temp = jsObject.main.temp;
+        let speed = jsObject.wind.speed;
+        let result = windChill(temp, speed);
+
+        document.querySelector('#windchill').textContent = result.toFixed(0);
+        function windChill(temp, speed) {
+            let result = 35.74  + (.6215 * temp) - (35.75 * Math.pow(speed, .16)) + (.4275 * temp * Math.pow(speed, .16));
+            return result;
+        
+    }
+        let current_temp = document.querySelector('#current');
+        current_temp.textContent = jsObject.weather[0].main;
+
+        let high_temp = document.querySelector('#high');
+        high_temp.textContent = jsObject.main.temp_max.toFixed(0);
+
+        let low_temp = document.querySelector('#low');
+        low_temp.textContent = jsObject.main.temp_min.toFixed(0);
+
+        let humidity = document.querySelector('#humidity');
+        humidity.textContent = jsObject.main.humidity;
+
+        let wind_speed = document.querySelector('#windspeed');
+        wind_speed.textContent = jsObject.wind.speed.toFixed(0);
+    });
+
---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,13 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=ab78af5b8d06381c9785194b99f21976";
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        document.getElementById('current-temp').textContent = jsObject.main.temp;
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+        const description = jsObject.weather[0].description;
+        document.getElementById('imagesrc').textContent = imagesrc;
+        document.getElementById('icon').setAttribute('src', imagesrc);
+        document.getElementById('icon').setAttribute('alt', description);
+    });
+
